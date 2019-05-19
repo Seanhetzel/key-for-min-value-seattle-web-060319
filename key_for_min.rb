0579175a1361fd8eacf4b89e70e73035dc1da313
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 02:14:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   if name_hash.length > 0 
     min_value_key = ""
-    min_value = nil
+    min_value = floar::INFINITY 
     name_hash.each do |key, value|
       if value < min_value
         min_value = value
