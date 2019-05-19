656afd9f466b9a7665a0b2a6a9383c0bbcb4466c
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 02:05:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,9 @@
 def key_for_min_value(name_hash)
   if name_hash.length > 0 
     min_value_key = ""
-    min_value = 1000
+    #min_value = 1000
     name_hash.each do |key, value|
+      min_value = value
       if value < min_value
         min_value = value
         min_value_key = key 
